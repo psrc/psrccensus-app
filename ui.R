8b0218f6d095cac542f0e042f24c9152ddb26b3e
Christy Lam <cyyllam@gmail.com>
2021-12-01 13:29:53 -0800

    added google fonts

--- ui.R
+++ ui.R
@@ -2,8 +2,10 @@ ui  <- fluidPage(
     useShinyjs(),
     tags$head(
         tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
-        tags$script(src = "js-script.js")
+        tags$script(src = "js-script.js"),
+        tags$link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Lato&family=Quicksand&family=Roboto&display=swap")
         ),
+    
     navbarPage(
         title = 'PSRC Census Explorer',
         
