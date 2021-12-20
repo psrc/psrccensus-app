84e7887011830175c54a0b055482b41423b896b8
Christy Lam <cyyllam@gmail.com>
2021-12-20 15:46:24 -0800

    removed browser

--- server.R
+++ server.R
@@ -321,7 +321,6 @@ server <- function(input, output, session) {
             if(input$table == 'B01001') {
                 age_groups <- c('Total', 'Total Female', 'Total Male', '0 to 4 years', '5 to 17 years', '18 to 64 years',
                                 '65 to 84 years', '85 years and over')
-                browser()
                 recs$grouping_fac <- factor(recs$grouping, levels = age_groups)
                 recs <- recs %>% arrange(name, grouping_fac)
             }
