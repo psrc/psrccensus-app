dfb1545bc18af02cfc98bfaf4a14cadde7049993
Christy Lam <cyyllam@gmail.com>
2022-01-31 15:24:31 -0800

    ungroup columns for aggregated results in map

--- ui.R
+++ ui.R
@@ -3,7 +3,8 @@ ui  <- fluidPage(
     tags$head(
         tags$link(rel = "stylesheet", type = "text/css", href = "styles.css"),
         tags$script(src = "js-script.js"),
-        tags$link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Lato&family=Quicksand&family=Roboto&display=swap")
+        tags$link(rel="stylesheet", href="https://fonts.googleapis.com/css2?family=Lato&family=Quicksand&family=Roboto&display=swap"),
+        tags$style(type = "text/css", "#main_map {height: calc(100vh - 320px) !important;}")
         ),
     
     navbarPage(
